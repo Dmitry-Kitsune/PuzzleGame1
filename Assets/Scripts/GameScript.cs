2c329c084665292b5ae37656a80ecf58dbf6a1e9
Dmitry Burlyaev <fuzzy1988@mail.ru>
2022-10-15 22:54:47 +0300

    Trying create puzzle with unity try.1

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    public GameObject EmptySpace;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
