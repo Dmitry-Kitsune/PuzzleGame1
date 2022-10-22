27e73f9bfdc2e2e87c27f52c94a01873c4e3793f
Dmitry Burlyaev <fuzzy1988@mail.ru>
2022-10-22 23:06:10 +0300

    Added the Script for tiles and fixed the positions of tiles on the board 22/10/2022 11:06

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
-    public GameObject EmptySpace;
-
+    [SerializeField] private Transform emptySpace = null;
+    private Camera _camera;
+    [SerializeField] private TilesScript[] tiles;
     void Start()
     {
-        
+        _camera = Camera.main;
     }
-
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit)
+            {
+               // Debug.Log(hit.transform.name);
+                if (Vector2.Distance(emptySpace.position, hit.transform.position) <= 6.01)
+                {
+                    Vector2 lastEmptySpacePosition = emptySpace.position;
+                    TilesScript thisTile = hit.transform.GetComponent<TilesScript>();
+                    emptySpace.position =  thisTile.targetPosition;
+                    thisTile.targetPosition = lastEmptySpacePosition;
+                }
+            }
+        }
     }
 }
